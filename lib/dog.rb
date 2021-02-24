21bbbeb356d86cc272502e017b7bc7c221ed796f
joemcpartland@gmail.com <josephmcpartland@JM.hsd1.co.comcast.net>
2021-02-24 09:43:50 -0700

    commit tree 8241fb7eac0a30898cf943bfa0f364c299494510

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
-# Add your code here
+require "pry"
+class Dog
+    attr_accessor :name
+    
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        puts @@all.map{ |dog| dog.name }
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+
+#     pluto = Dog.new("pluto")
+#     fido = Dog.new("fido")
+#     maddy = Dog.new("maddy")
+
+# binding.pry
+end
